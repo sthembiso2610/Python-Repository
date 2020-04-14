b8f7cdaf267018a70b21623e8e179ed70102d91f
Sthembiso Tshabalala <sthembiso2698@gmail.com>
2020-04-14 14:44:35 +0200

    eslint

--- src/estimator.py
+++ src/estimator.py
@@ -1,5 +1,8 @@
+
+
 def estimator(data):
-  input = data
-  impact.currentlyInfected = input.reportedCases * 10
-  severeImpact.currentlyInfected = input.reportedCases * 50 
-  return data 
+    impact = data
+    severeImpact = data
+    impact.currentlyInfected = input.reportedCases * 10
+    severeImpact.currentlyInfected = input.reportedCases * 50
+    return data 
