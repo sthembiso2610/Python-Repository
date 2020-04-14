c8de20af8e9f27367f088fc19a53716266e645ac
Sthembiso Tshabalala <sthembiso2698@gmail.com>
2020-04-14 13:58:36 +0200

    try

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,5 @@
 def estimator(data):
-  return data
+  input = data
+  impact.currentlyInfected = input.reportedCases * 10
+  severeImpact.currentlyInfected = input.reportedCases * 50 
+  return data 
